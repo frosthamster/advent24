d0ce9f0c1a08723e8bdeb3e07110bc622c4cd05d
Danil Anisimov <anisimov_d@joom.com>
2024-12-01 12:40:31 +0000

    day 01

--- src/01.zig
+++ src/01.zig
@@ -1,11 +1,64 @@
 const std = @import("std");
 
-pub fn solve(_: std.mem.Allocator) !void {
-    const fd = try std.fs.cwd().openFile("inputs/01.input", .{});
+pub fn solve1(alloc: std.mem.Allocator, path: []const u8) !void {
+    var left = std.ArrayList(u32).init(alloc);
+    var right = std.ArrayList(u32).init(alloc);
+    defer left.deinit();
+    defer right.deinit();
+    try parse(path, &left, &right);
+
+    std.sort.pdq(u32, left.items, {}, comptime std.sort.asc(u32));
+    std.sort.pdq(u32, right.items, {}, comptime std.sort.asc(u32));
+
+    var sum: u32 = 0;
+    for (left.items, right.items) |u, v| {
+        sum += dist(u, v);
+    }
+    std.debug.print("011: {any}\n", .{sum});
+}
+
+pub fn solve2(alloc: std.mem.Allocator, path: []const u8) !void {
+    var left = std.ArrayList(u32).init(alloc);
+    var right = std.ArrayList(u32).init(alloc);
+    defer left.deinit();
+    defer right.deinit();
+    try parse(path, &left, &right);
+
+    var counter = std.AutoHashMap(u32, u32).init(alloc);
+    defer counter.deinit();
+    for (right.items) |v| {
+        const res = try counter.getOrPut(v);
+        if (res.found_existing) {
+            res.value_ptr.* += 1;
+        } else {
+            res.value_ptr.* = 1;
+        }
+    }
+
+    var sum: u32 = 0;
+    for (left.items) |u| {
+        sum += u * (counter.get(u) orelse 0);
+    }
+    std.debug.print("012: {any}\n", .{sum});
+}
+
+fn parse(path: []const u8, left: *std.ArrayList(u32), right: *std.ArrayList(u32)) !void {
+    const fd = try std.fs.cwd().openFile(path, .{});
     defer fd.close();
 
     var buf: [32]u8 = undefined;
     while (try fd.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        std.debug.print("{s}\n", .{line});
+        var splitted = std.mem.splitSequence(u8, line, "   ");
+        const u = try std.fmt.parseInt(u32, splitted.next().?, 10);
+        const v = try std.fmt.parseInt(u32, splitted.next().?, 10);
+        try left.append(u);
+        try right.append(v);
     }
+}
+
+fn dist(u: u32, v: u32) u32 {
+    if (u > v) {
+        return u - v;
+    }
+    return v - u;
 }
--- src/main.zig
+++ src/main.zig
@@ -11,10 +11,26 @@ pub fn main() !void {
     var args_it = try std.process.argsWithAllocator(alloc);
     defer args_it.deinit();
 
+    var args = std.ArrayList([]const u8).init(alloc);
+    defer args.deinit();
+
     _ = args_it.skip();
     while (args_it.next()) |arg| {
-        if (std.mem.eql(u8, arg, "01")) {
-            try d01.solve(alloc);
-        }
+        try args.append(arg);
     }
+
+    const debug = args.items.len > 1 and std.mem.eql(u8, args.items[1], "--debug");
+    if (std.mem.eql(u8, args.items[0], "011")) {
+        try d01.solve1(alloc, path(debug, "inputs/01.input"));
+    }
+    if (std.mem.eql(u8, args.items[0], "012")) {
+        try d01.solve2(alloc, path(debug, "inputs/01.input"));
+    }
+}
+
+fn path(debug: bool, p: []const u8) []const u8 {
+    if (debug) {
+        return "inputs/debug.input";
+    }
+    return p;
 }
